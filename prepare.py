91d3125d8aaa322f7f26881e5d777346fa4ebca3
CodyBrettWatson <cody.brett.watson@gmail.com>
2019-04-03 15:47:00 -0500

    add prepare.py v2

--- prepare.py
+++ prepare.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import math as m
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
@@ -34,6 +36,46 @@ col = ['gender',
 
 def prep_telco(df_telco):
     df = df_telco.copy()
+
+    df['tenure_year']= (df['tenure']/12).round(decimals=2)
+    df['in_tenure_year']= np.ceil(df['tenure']/12)
+
+    df['family_plan'] = (df.partner == 'Yes') & (df.dependents == 'Yes') & (df.multiple_lines == 'Yes')
+    df.loc[df.family_plan == True, 'family_plan'] = 1
+    df.loc[df.family_plan == False, 'family_plan'] = 0
+
+    conditions_1 = [
+    (df['dependents'] == 'No') & (df['partner'] == 'No'),
+    (df['dependents'] == 'Yes') & (df['partner'] == 'No'),
+    (df['dependents'] == 'No') & (df['partner'] == 'Yes'),
+    (df['dependents'] == 'Yes') & (df['partner'] == 'Yes')]
+    choices_1 = [0,1,2,3]
+    df['household'] = np.select(conditions_1, choices_1)
+
+    conditions_2 = [
+    (df['multiple_lines'] == 'Yes') & (df['phone_service'] == 'Yes'),
+    (df['multiple_lines'] == 'No') & (df['phone_service'] == 'Yes'),
+    (df['multiple_lines'] == 'No phone service') & (df['phone_service'] == 'No'),]
+    choices_2 = [2,1,0]
+    df['phone_id'] = np.select(conditions_2, choices_2)
+
+    conditions_3 =[
+    (df['streaming_tv']=='No internet service')& (df['streaming_movies']=='No internet service'),
+    (df['streaming_tv']=='No')& (df['streaming_movies']=='No'),
+    (df['streaming_tv']=='No')& (df['streaming_movies']=='Yes'),
+    (df['streaming_tv']=='Yes')& (df['streaming_movies']=='No'),
+    (df['streaming_tv']=='Yes')& (df['streaming_movies']=='Yes')]
+    choices_3 = [0,1,2,3,4]
+    df['streaming_services'] = np.select(conditions_3, choices_3)
+
+    conditions_4 =[
+    (df['online_security']=='No internet service')& (df['online_backup']=='No internet service'),
+    (df['online_security']=='No')& (df['online_backup']=='No'),
+    (df['online_security']=='No')& (df['online_backup']=='Yes'),
+    (df['online_security']=='Yes')& (df['online_backup']=='No'),
+    (df['online_security']=='Yes')& (df['online_backup']=='Yes')]
+    choices_4 = [0,1,2,3,4]
+    df['online_security_backup'] = np.select(conditions_4, choices_4)
 
     encoder_payment_type = LabelEncoder()
     encoder_payment_type.fit(df.payment_type)
@@ -98,8 +140,9 @@ def prep_telco(df_telco):
     encoder_partner = LabelEncoder()
     encoder_partner.fit(df.partner)
     df = df.assign(partner_encode=encoder_partner.transform(df.partner))
-    
+
     return df
+
 
 
 

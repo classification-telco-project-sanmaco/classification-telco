d276ba6356340e8317a3a5df0e74dcf88aa88ac4
CodyBrettWatson <cody.brett.watson@gmail.com>
2019-04-04 08:44:12 -0500

    add prepare v4.0

--- prepare.py
+++ prepare.py
@@ -5,6 +5,9 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
+# This is the list of object columns from the Telco_DB
+# These would need to be encoded if they need to be used as some form of metric
+
 col = ['gender',
  'partner',
  'dependents',
@@ -23,16 +26,30 @@ col = ['gender',
  'internet_service_type',
  'payment_type']
 
-# def prep_telco_data(df):
-#     df['total_charges'] = df['total_charges'].convert_objects(convert_numeric=True)
-#     df.total_charges.dropna(0, inplace=True)
-#     for col in df.drop(columns=(['customer_id', 'total_charges', 'monthly_charges'])):
-# #         print('encoding ' + col) -- error checking
-#         encoder = LabelEncoder()
-#         encoder.fit(df[col])
-#         new_col = col + '_e'
-#         df[new_col] = encoder.transform(df[col])
-#     return df
+
+# This is ORION's for-loop for the label encoder
+# Something went wrong when i tried using it for our purposes
+# It was giving 3 sets of label encoded fields for every field regardless of whether or not it was an object or int
+
+def prep_telco_data(df):
+    df['total_charges'] = df['total_charges'].convert_objects(convert_numeric=True)
+    df.total_charges.dropna(0, inplace=True)
+    for col in df.drop(columns=(['customer_id', 'total_charges', 'monthly_charges'])):
+    #  print('encoding ' + col) -- error checking
+        encoder = LabelEncoder()
+        encoder.fit(df[col])
+        new_col = col + '_e'
+        df[new_col] = encoder.transform(df[col])
+    return df
+
+
+
+#This is the main Telco DB prep function
+# Converts total_charges to a float from a string
+# Gives tenure as a year
+# Gives a family plan which by our means is a custm=omer who has a dependent, partner, and multiple lines
+# Gives household, phone_id, streaming_services, and online_security fields. Which are a combination of other fields of like groups
+# Encodes all object fields
 
 def prep_telco(df_telco):
     df = df_telco.copy()
@@ -40,6 +57,8 @@ def prep_telco(df_telco):
     df.total_charges = pd.to_numeric(df.total_charges, downcast='float', errors='coerce')
 
     df['tenure_year']= (df['tenure']/12).round(decimals=2)
+    # this gives the 'year' that the customer is in i.e. 1st year 2nd year etc. 
+    # so if they are 0.75 years tenure they are in the 1st year if they are 3.3 in the 4th year
     df['in_tenure_year']= np.ceil(df['tenure']/12)
 
     df['family_plan'] = (df.partner == 'Yes') & (df.dependents == 'Yes') & (df.multiple_lines == 'Yes')
@@ -145,12 +164,15 @@ def prep_telco(df_telco):
 
     return df
 
-####
+
+# This is a train_test_split function that should be used after all the data prep has been done i.e. using the prep_telco function
 def split_telco(df_telco):
     return train_test_split(df_telco, train_size=0.7, 
     random_state=123, stratify=df_telco[["churn"]])
 
-
+# This function need to be fixed
+# When calling this function in another file (jupyter notebook) the selected dataframe doesn't become scaled or transformed.
+# Took the guts of this function and put it into the notebook and it worked
 def min_max_scale_telco(df_train, df_test):
     df_train_scaled = df_train.copy()
     df_test_scaled = df_test.copy()
@@ -160,5 +182,12 @@ def min_max_scale_telco(df_train, df_test):
     
     df_train_scaled[['monthly_charges', 'total_charges']] = scaler.fit_transform(df_train[['monthly_charges', 'total_charges']])
     df_test_scaled[['monthly_charges', 'total_charges']] = scaler.fit_transform(df_test[['monthly_charges', 'total_charges']])
-    
+        
+    ## This is the code that worked in the jupyter notebook
+    # scaler = MinMaxScaler()
+    # scaler.fit(train_df[['monthly_charges', 'total_charges']])
+
+    # train_df[['monthly_charges', 'total_charges']] = scaler.transform(train_df[['monthly_charges', 'total_charges']])
+    # test_df[['monthly_charges', 'total_charges']] = scaler.transform(test_df[['monthly_charges', 'total_charges']])
+
     return df_train_scaled, df_test_scaled
